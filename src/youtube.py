938bbc3cd131be7b963e12e2fee52df5cf30a288
thiemo-seys <55407731+thiemo-seys@users.noreply.github.com>
2024-01-14 14:29:19 +0100

    improve audio quality download

--- src/youtube.py
+++ src/youtube.py
@@ -4,4 +4,4 @@ from pytube import YouTube
 def download_audio(url: str, output_path: str) -> str:
     """Returns the path to the downloaded audio file"""
     yt = YouTube(url)
-    return yt.streams.filter(only_audio=True).first().download(output_path=output_path)
+    return yt.streams.get_audio_only().download(output_path=output_path)
