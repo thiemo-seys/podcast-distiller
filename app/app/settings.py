a38d6ab6f583d24e00509b2023474871b23fb5f0
thiemo-seys <55407731+thiemo-seys@users.noreply.github.com>
2024-03-02 13:15:05 +0100

    dockerize django

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +77,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "podcast_distiller"),
+        "USER": os.environ.get("POSTGRES_USER", "podcast_distiller"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "podcast_distiller"),
+        "HOST": "postgres",
+        "PORT": "5432",
     }
 }
 
