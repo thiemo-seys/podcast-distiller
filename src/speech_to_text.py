09f84aaba418480d5121ce78166b2cce94cfe6ae
thiemo-seys <55407731+thiemo-seys@users.noreply.github.com>
2024-08-12 20:20:16 +0200

    use whisper for text to speech

---
+++ src/speech_to_text.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+import numpy as np
+import whisper
+
+
+def list_models() -> list[str]:
+    """List all available models"""
+    return whisper.available_models()
+
+
+def get_model(model_name: str) -> whisper.Whisper:
+    """Load the whisper given model"""
+    return whisper.load_model(model_name)
+
+
+def transcribe(model: whisper.Whisper, audio: Union[str, np.ndarray]) -> str:
+    return model.transcribe(audio=audio)
+
+
+class Transcriber:
+    def __init__(self, model: whisper.Whisper = None):
+        self.model = model
